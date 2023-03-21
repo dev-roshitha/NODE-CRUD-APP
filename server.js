0c2d4397730859346e4c39a2b0ae8cdc9e61bca7
Roshitha Ranasinghe <sajithroshitha98@gmail.com>
2023-03-21 23:58:53 +0530

    setup express routes

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log("Hello world")
+const express = require('express')
+const app = express()
+
+//Routes
+app.get("/", (req, res) => {
+    res.send("Welcome to NODE CRUD")
+})
+
+app.listen(8080, () => {
+    console.log(`App running on http://localhost:8080`)
+})
