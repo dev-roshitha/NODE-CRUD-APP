1f66b54065c618b6a403e2f41e4ada97fa0da297
Roshitha Ranasinghe <sajithroshitha98@gmail.com>
2023-03-22 23:52:46 +0530

    connect mongodb

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
 
+mongoose.connect('mongodb+srv://rosh:<password>@simplemern.p6hq46y.mongodb.net/?retryWrites=true&w=majority')
+
 //Routes
 app.get("/", (req, res) => {
     res.send("Welcome to NODE CRUD")
