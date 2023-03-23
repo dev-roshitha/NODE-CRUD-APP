7afdebe4192f8104ed9c4a6fbf964728009341c7
Roshitha Ranasinghe <sajithroshitha98@gmail.com>
2023-03-23 23:51:44 +0530

    update

--- server.js
+++ server.js
@@ -29,6 +29,18 @@ app.get("/products", async(req, res) => {
     }
 })
 
+app.get("/products/:id", async(req, res) => {
+    try {
+        const {id} = req.params
+        const product = await Product.findById(id)
+        res.status(200).json(product)
+        
+    } catch (error) {
+        res.status(500).json({message: error.massage})
+        console.log(error)
+    }
+})
+
 app.post("/products", async(req, res) => {
     try{
         const product = await Product.create(req.body)
