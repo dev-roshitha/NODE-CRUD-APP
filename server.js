d730bd97340419173f23b499a517cae42f0000b4
Roshitha Ranasinghe <sajithroshitha98@gmail.com>
2023-03-23 23:33:43 +0530

    update

--- server.js
+++ server.js
@@ -21,11 +21,11 @@ app.get("/", (req, res) => {
 app.post("/product", async(req, res) => {
     try{
 
-        const product = await Product(req.body)
+        const product = await Product.create(req.body)
         res.status(200).json(product)
 
     }catch(error){
-        console.log(error)
+        console.log(error.message)
         res.status(500).json({
             message: error.message
         })
