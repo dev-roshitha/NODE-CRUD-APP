637e5bf2a40f02679d56d59166a14d38960a352f
Roshitha Ranasinghe <sajithroshitha98@gmail.com>
2023-03-25 13:26:54 +0530

    update with view

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const router = express.Router()
-const Product = require("./models/productModel")
+const Product = require("../models/productModel")
 const mongoose = require("mongoose")
 
 // Root Route
--- server.js
+++ server.js
@@ -21,72 +21,72 @@ mongoose.connect('mongodb+srv://rosh:rosh123@cluster0.rj7ysis.mongodb.net/?retry
 app.use("", require("./routes/routes"))
 
 
-app.get("/products", async(req, res) => {
-    try {
-        const products = await Product.find({})
-        res.status(200).json(products)
+// app.get("/products", async(req, res) => {
+//     try {
+//         const products = await Product.find({})
+//         res.status(200).json(products)
 
-    } catch (error) {
-        res.status(500).json({message: error.massage})
-        console.log(error)
-    }
-})
+//     } catch (error) {
+//         res.status(500).json({message: error.massage})
+//         console.log(error)
+//     }
+// })
 
-app.get("/products/:id", async(req, res) => {
-    try {
-        const {id} = req.params
-        const product = await Product.findById(id)
-        res.status(200).json(product)
+// app.get("/products/:id", async(req, res) => {
+//     try {
+//         const {id} = req.params
+//         const product = await Product.findById(id)
+//         res.status(200).json(product)
         
-    } catch (error) {
-        res.status(500).json({message: error.massage})
-        console.log(error)
-    }
-})
+//     } catch (error) {
+//         res.status(500).json({message: error.massage})
+//         console.log(error)
+//     }
+// })
 
-app.put("/products/:id", async(req, res) => {
-    try {
-        const {id} = req.params
-        const product = await Product.findByIdAndUpdate(id, req.body)
-        if(!product){
-            return res.status(404).json({message: `cannot find product with ${id}`})
-        }
-        const updatedProduct = await Product.findById(id)
-        res.status(200).json(updatedProduct)
+// app.put("/products/:id", async(req, res) => {
+//     try {
+//         const {id} = req.params
+//         const product = await Product.findByIdAndUpdate(id, req.body)
+//         if(!product){
+//             return res.status(404).json({message: `cannot find product with ${id}`})
+//         }
+//         const updatedProduct = await Product.findById(id)
+//         res.status(200).json(updatedProduct)
 
-    } catch (error) {
-        res.status(500).json({message: error.message})
-        console.log(error)
-    }
-})
+//     } catch (error) {
+//         res.status(500).json({message: error.message})
+//         console.log(error)
+//     }
+// })
 
-app.delete("/products/:id", async(req, res) => {
-    try {
-        const {id} = req.params
-        const product = await Product.findByIdAndDelete(id)
-        if(!product){
-            return res.status(404).json({message: `cannot find product with ${id}`})
-        }
-        res.status(200).json(product)
+// app.delete("/products/:id", async(req, res) => {
+//     try {
+//         const {id} = req.params
+//         const product = await Product.findByIdAndDelete(id)
+//         if(!product){
+//             return res.status(404).json({message: `cannot find product with ${id}`})
+//         }
+//         res.status(200).json(product)
 
-    } catch (error) {
-        res.status(500).json({message: error.message})
-        console.log(error)
-    }
-})
+//     } catch (error) {
+//         res.status(500).json({message: error.message})
+//         console.log(error)
+//     }
+// })
 
-app.post("/products", async(req, res) => {
-    try{
-        const product = await Product.create(req.body)
-        res.status(200).json(product)
+// app.post("/products", async(req, res) => {
+//     try{
+//         const product = await Product.create(req.body)
+//         res.status(200).json(product)
 
-    }catch(error){
-        console.log(error.message)
-        res.status(500).json({
-            message: error.message
-        })
-    }
-})
+//     }catch(error){
+//         console.log(error.message)
+//         res.status(500).json({
+//             message: error.message
+//         })
+//     }
+// })
 
 app.listen(8080, () => {
     console.log(`App running on http://localhost:8080`)
