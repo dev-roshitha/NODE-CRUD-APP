219b773bad553930cccc16aa6a7a49859dcf4c23
Roshitha Ranasinghe <sajithroshitha98@gmail.com>
2023-03-25 15:13:53 +0530

    insert records with frontend

--- models/productModel.js
+++ models/productModel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const productSchema = mongoose.Schema(
+const productSchema = new mongoose.Schema(
     {
         name: {
             type: String,
@@ -17,7 +17,7 @@ const productSchema = mongoose.Schema(
         },
         image: {
             type: String,
-            required: false
+            required: true
         }
     },
     { 
--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,39 @@ const express = require("express")
 const router = express.Router()
 const Product = require("../models/productModel")
 const mongoose = require("mongoose")
+const multer = require('multer')
+
+//Image upload
+let storage = multer.diskStorage({
+    destination: function(req ,file, cb){
+        cb(null, './uploads')
+    }, 
+    filename: function(req, file, cb){
+        cb(null, file.fieldname+"_"+Date.now()+"_"+file.originalname)
+    }
+})
+
+let upload = multer({
+    storage: storage,
+}).single("image")
+
+//insert records to database
+router.post("/add", upload, async(req, res) => {
+    try {
+        const product = await Product.create({
+            name: req.body.name,
+            quantity: req.body.quantity,
+            price: req.body.price,
+            image: req.file.filename
+        })
+        console.log(`product added successfully ${product}`)
+        res.redirect("/")
+
+    } catch (error) {
+        res.json({message: error.message, type: 'danger'})
+        console.log(error)
+    }
+})
 
 // Root Route
 router.get("/", (req, res) => {
